6faf3c670d1da043585f912e70f306c26dc91618
zinozzing <rlawlsgh1111@naver.com>
2022-01-11 20:22:07 +0900

    fin geo cont

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+const API_KEY = "0d3cc914979c6be87b62d144d6daf203";
+
+function onGeoOK(position) {
+  const lat = position.coords.latitude;
+  const lng = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOK, onGeoError);
