9f13a30c53b79627ab743f2a2d50ea6810785c0f
zinozzing <rlawlsgh1111@naver.com>
2022-01-11 20:44:09 +0900

    Add env

--- js/weather.js
+++ js/weather.js
@@ -1,8 +1,7 @@
-const API_KEY = "0d3cc914979c6be87b62d144d6daf203";
-
 function onGeoOK(position) {
   const lat = position.coords.latitude;
   const lng = position.coords.longitude;
+  const API_KEY = globalVariable.API_KEY;
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
   fetch(url)
     .then(response => response.json())
