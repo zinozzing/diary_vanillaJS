52df622cee302e5567abcfaa2d820b2fe9fa2ac2
zinozzing <rlawlsgh1111@naver.com>
2022-01-28 01:19:12 +0900

    Update record feeling list

--- js/todo.js
+++ js/todo.js
@@ -1,6 +1,6 @@
 const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
-const toDoList = document.getElementById("todo-list");
+const toDoList = document.getElementById("check-list");
 
 const TODOS_KEY = "todos";
 
